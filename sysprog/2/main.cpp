169fa07b664c284159c8dc46dd2e0b688d45803d
axel36 <khramov@alexey.by>
2019-04-08 00:41:18 +0300

    pipe...

--- sysprog/2/main.cpp
+++ sysprog/2/main.cpp
@@ -14,7 +14,7 @@ char * get_cmd(void) {
     size_t lenmax = 100, len = lenmax;
     int c;
 
-    printf("$>");
+
     if(line == NULL)
         return NULL;
 
@@ -45,17 +45,17 @@ char * get_cmd(void) {
 int main() {
     const char *ext = "exit\n";
     for(;;){
-        char *line = get_cmd();
-        int l = strlen(line));
-        if(line[l-1]=='\\   ')
-        if (strcmp(line, ext) == 0){
+        //char *line = get_cmd();
+        printf("$>");
+        fflush(stdout);
+        cmd_line cmds = parse_line();
+        if (strcmp(cmds.commands[0].name, ext) == 0){
             break;
         }
 
-        cmd_line cmds = parse_line(line);
 
         if(cmds.count == 1){
-//            printf("name - %s\narg - %s\n",cmds.commands[0].name, cmds.commands[0].argv[1]);
+            printf("name - %s\narg - %s\n",cmds.commands[0].name, cmds.commands[0].argv[1]);
             pid_t pid = fork();
             if(pid == -1){
                 printf("\n bad fork");
@@ -78,66 +78,68 @@ int main() {
 //
         } else {
 
-            int ** pipefds = (int **)malloc((cmds.count - 1)* sizeof(int*)); // 0 = read, 1 = write
-            for(int i = 0; i< cmds.count-1; i++){
-                int tmp[2];
-                if (pipe(tmp) < 0) {
+            int in_fd, out_fd;
+            for (int i = 0; i < cmds.count; i++) {
+                int pipefd[2];// 0 = read, 1 = write
+
+                if (pipe(pipefd) < 0) {
                     printf("\nPipe could not be initialized");
                 }
-                pipefds[i] = tmp;
-            }
-            pid_t pd1, pd2;
 
-            pd1 = fork();
-            if (pd1 < 0) {
-                printf("\nbad fork");
-            }
 
-            if (pd1 == 0) {
-                close(pipefds[0][0]);
-                dup2(pipefds[0][1], STDOUT_FILENO);
-                close(pipefds[0][1]);
-
-                if (execvp(cmds.commands[0].name, cmds.commands[0].argv) < 0) {
-                    printf("\nCould not execute command 1..");
-                    exit(0);
+                if (i > 0) {
+                    dup2(in_fd, pipefd[0]);
                 }
-                exit(0);
-            } else {
-                // Parent executing
+                in_fd = pipefd[1];
+//                printf("proc%d: infd =%d\n", i, in_fd);
+//                fflush(stdout);
 
-                pd2 = fork();
+                if (fork() == 0) { // in child
+                    // 0 = read, 1 = write
+                    printf("proc%d: in\n", i);
+                    fflush(stdout);
+                    dup2(pipefd[0], STDIN_FILENO);
+                    close(pipefd[0]);
 
-                if (pd2 < 0) {
-                    printf("\nCould not fork");
-                }
 
-                // Child 2 executing..
-                // It only needs to read at the read end
-                if (pd2 == 0) {
-                    close(pipefds[0][1]);
-                    dup2(pipefds[0][0], STDIN_FILENO);
-                    close(pipefds[0][0]);
-                    if (execvp(cmds.commands[1].name, cmds.commands[1].argv) < 0) {
-                        printf("\nCould not execute command 2..");
+                    dup2(pipefd[1], STDOUT_FILENO);
+                    close(pipefd[1]);
+
+                    if (cmds.commands[i].argc > 0) {
+                        if (execvp(cmds.commands[i].name, cmds.commands[i].argv) < 0) {
+                            printf("\nbad execv\n");
+                        }
+                        printf("%s", cmds.commands[i].argv[1]);
+                    } else {
+                        if (execlp(cmds.commands[i].name, cmds.commands[i].name, NULL) < 0) {
+                            printf("\nbad execl for cmd: _%s_\n", cmds.commands[0].name);
+                        }
                     }
+
+                    close(pipefd[1]);
+                    close(pipefd[0]);
                     exit(0);
+
                 } else {
-                    // parent executing, waiting for two children
-                    close(pipefds[0][1]);
-                    wait(NULL);
-                    wait(NULL);
+                    close(pipefd[1]);
+                }
+
+            }
+
+            for (int i = 0; i < cmds.count; i++) {
+                wait(NULL);
+            }
+
+            for (int i = 0; i < cmds.count; i++) {
+
+                printf("your cmd name is: %s \n", cmds.commands[i].name);
+                for (int j = 0; j < cmds.commands[i].argc; j++) {
+                    printf("\t param%d is: %s\n", j, cmds.commands[i].argv[j]);
                 }
             }
-//            for (int i = 0; i < cmds.count; i++) {
-//
-//                printf("your cmd name is: %s \n", cmds.commands[i].name);
-//                for (int j = 0; j < cmds.commands[i].argc; j++) {
-//                    printf("\t param%d is: %s\n", j, cmds.commands[i].argv[j]);
-//                }
-//            }
         }
     }
+
 
     printf("\nBye!");
     return 0;
--- sysprog/2/parser.cpp
+++ sysprog/2/parser.cpp
@@ -70,37 +70,178 @@ cmd parse_command(char *line, int start, int end) {
     return command;
 }
 
-cmd_line parse_line(char *line) {
-    int count = 1;
-    cmd *cmds = (cmd *) malloc(count * sizeof(cmd));
+void alloc1(char * &str, int &max_len, int len){
+    if(len == max_len){
+        max_len*=2;
+        char *tmp = (char *) malloc(max_len * sizeof(char));
+        memcpy(tmp, str, len);
+        free(str);
+        str = tmp;
+    }
+}
+
+
+void add_char(char **str, int *max_len, int len, char c){
+    if(len+1 == *max_len){
+        *max_len = *max_len*2;
+        *str =(char *) realloc(*str, *max_len * sizeof(char));
+    }
+    (*str)[len+1] = c;
+}
+
+
+
+cmd_line parse_line(void) {
+    // c1 p1 p2 | c2 p3 p4
+    int tokens_count = 0;
+    int cmd_count = 1;
     int start = 0;
     int j = 0;
     int max_len = 3;
     int len = 0;
-    char **tokens = (char**)malloc(max_len * sizeof(char));
-    int token_max_len = 4;
-    int token_len = 0;
-    char *token = (char*)malloc( token_max_len* sizeof(char));
-    for (int i = 0; i <= strlen(line); i++) {
+    char **tokens = (char**)malloc(tokens_count * sizeof(char));
+    char *token = (char*)malloc( max_len* sizeof(char));
+    int c = 0;
+    char prev = 0;
+    char new_char =0;
+    while (true) {
+        c = getchar();
+        if(c == EOF || c == '\n')
+            break;
+        switch(c) {
+            case ' ':
+                token[j] = 0;
+                tokens[tokens_count] = token;
+                tokens_count++;
+                tokens = (char **) realloc(tokens, (tokens_count + 1) * sizeof(char *));
+                max_len = 5;
+                token = (char *) malloc((max_len) * sizeof(char));
+                j = 0;
+                prev = (char)c;
+                break;
+            case '\"':
+                prev = 0;
+                while(true){
+                    c = getchar();
+                    if(c == EOF) {
+                        break;
+                    }
+                    if(c == '\n'){
+                        printf(">");
+                    }
+                    if(c != '\"' || prev == '\\'){
+                        token[j] = (char)c;
+                        j++;
+                        alloc1(token, max_len, j);
+                        prev = (char)c;
+                    }else{
+                        token[j] = 0;
+                        tokens[tokens_count] = token;
+                        tokens_count++;
+                        tokens = (char **) realloc(tokens, (tokens_count + 1) * sizeof(char *));
+                        max_len = 5;
+                        token = (char *) malloc((max_len) * sizeof(char));
+                        j = 0;
+                        break;
+                    }
+                }
+                break;
+            case '>':
+                break;
+            case '\'':
 
-        if (line[i] == '|') {
-            // > cmd | cmd2
-            //  0123456789
-            // i = 5;
-            cmds[count - 1] = parse_command(line, start, i - 1);
+                while(true){
+                    c = getchar();
+                    if(c == EOF) {
+                        break;
+                    }
+                    if(c == '\n'){
+                        printf(">");
+                        continue;
+                    }
+                    if(c != '\'' || prev == '\\'){
+                        token[j] = (char)c;
+                        j++;
+                        alloc1(token, max_len, j);
+                        prev = (char)c;
+                    }else{
+                        token[j] = 0;
+                        tokens[tokens_count] = token;
+                        tokens_count++;
+                        tokens = (char **) realloc(tokens, (tokens_count + 1) * sizeof(char *));
+                        max_len = 5;
+                        token = (char *) malloc((max_len) * sizeof(char));
+                        j = 0;
+                        break;
+                    }
+                }
+                prev = (char)c;
+                break;
+            case '|':
+                cmd_count++;
+                token[j] = (char)c;
+                j++;
+                alloc1(token, max_len, j);
+                prev = (char)c;
+                break;
 
-            cmds = (cmd *) realloc(cmds, (count + 1) * sizeof(cmd));
-            start = i + 1;
-            count++;
+            case '\\':
+                new_char = (char)getchar();
+                if (new_char =='\n'){
+                    continue;
+                }
+                token[j] = (char)c;
+                j++;
+                alloc1(token, max_len, j);
+                prev = new_char;
+                break;
+
+            default:
+                token[j] = (char)c;
+                j++;
+                alloc1(token, max_len, j);
+                prev = (char)c;
+        }
+
+    }
+    if (j > 0) {
+        token[j] = 0;
+        tokens[tokens_count] = token;
+        tokens_count++;
+    }
+
+    cmd *cmds = (cmd *) malloc(cmd_count * sizeof(cmd));
+    cmd command{};
+    char **argv = (char **)malloc((tokens_count + 1) * sizeof(char *));
+    j = 0;
+    int cmd_num = 0;
+    for (int i = 0;i< tokens_count; i++){
+        printf("t%d - %s\n",i,tokens[i]);
+
+        if(strcmp(tokens[i], "|") == 0){ // lines are equal
+            argv[j] = NULL;
+            cmds[cmd_num].argv = argv;
+            cmds[cmd_num].name = argv[0];
+            cmds[cmd_num].argc = j;
+
+            cmd_num++;
+            j = 0;
+            argv = (char **)malloc((tokens_count + 1 - i)* sizeof(char *));
+        }else {
+            argv[j] = tokens[i];
             j++;
         }
+
     }
-    cmds[j] = parse_command(line, start, strlen(line) - 1);
-//    printf("p-%s\n",line);
+
+    argv[j] = NULL;
+    cmds[cmd_num].argv = argv;
+    cmds[cmd_num].name = argv[0];
+    cmds[cmd_num].argc = j;
 
 
     cmd_line commands{};
-    commands.count = count;
+    commands.count = cmd_count;
     commands.commands = cmds;
     return commands;
 }
